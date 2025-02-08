47580a3087d257ee1b42b00ddf01a499c8d80820
Haresh Kurade <108918863+Haresh-729@users.noreply.github.com>
2025-02-09 02:35:31 +0530

    report apis added

--- Backend/routes/report.js
+++ Backend/routes/report.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { listBills, getBillDetails } = require("../controllers/report");
+const { listBills, getBillDetails, listUserBils } = require("../controllers/report");
 const authMiddleware = require("../middlewares/auth");
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 //all before login
 router.get("/get-collection-bills", authMiddleware, listBills);
 router.get("/get-single-bill-details", authMiddleware, getBillDetails);
+router.get("/get-user-bills", authMiddleware, listUserBils);
 
 module.exports = router;
